f917a56c1d8563f20675d88192df2e951f087e79
Helg Bir <helgooni@yandex.ru>
2026-05-24 21:05:43 +0300

    Commit for Sprint_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,8 @@ public class Main {
         Meat meat = new Meat(5, 100.0);
         Apple redApple = new Apple(10, 50.0, Colour.RED);
         Apple greenApple = new Apple(8, 60.0, Colour.GREEN);
-
-        Food[] products = {meat, redApple, greenApple, redApple, meat, greenApple};
-
-
+        Food[] products = {meat, redApple, greenApple};
         ShoppingCart cart = new ShoppingCart(products);
-
         System.out.println("Общая сумма товаров без скидки: "
                 + cart.getTotalPriceNotDiscount());
         System.out.println("Общая сумма товаров со скидкой: "
--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -1,31 +1,21 @@
 package model;
 import model.constants.Colour;
-
-
 import static model.constants.Discount.DISCOUNT;
 
-
-public class Apple extends Food implements Discountable {
+public class Apple extends Food {
     public String color;
     private double appleDiscount;
     public Apple(int amount, double price, String color){
         super(amount, price);
         this.color = color;
         this.isVegetarian = true;
-
-
     }
 
     @Override
-    public double getDiscount() {
-        if (Colour.RED == color) {
-            appleDiscount = DISCOUNT;
-        } else appleDiscount = 0.0;
-        return 0.0;
-    }
+    public double getDiscount() { return Colour.RED.equals(color) ? DISCOUNT : 0.0; }
 
     public double getAppleDiscount(){
-        getDiscount();
+        appleDiscount = getDiscount();
         return appleDiscount;
     }
 
